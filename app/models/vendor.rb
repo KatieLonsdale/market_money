974ceebc6b85a336e57983759ec6e670fc217688
Katie Lonsdale <katie952@gmail.com>
2023-05-16 12:06:47 -0600

    feat: add find_vendor method

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,13 @@
 class Vendor < ApplicationRecord
   has_many :market_vendors
   has_many :markets, through: :market_vendors
+
+  def self.find_vendor(id)
+    vendor = Vendor.find_by(id: id)
+    if vendor
+      vendor
+    else
+      ErrorVendor.new("Couldn't find Vendor with 'id'=#{id}")
+    end
+  end
 end
