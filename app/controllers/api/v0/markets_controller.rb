4662dd537d1c48ee636b6ad25e98bdcc8f5a7b3f
Katie Lonsdale <katie952@gmail.com>
2023-05-15 21:04:21 -0600

    feat: add show action

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -2,4 +2,13 @@ class Api::V0::MarketsController < ApplicationController
   def index
     render json: MarketSerializer.new(Market.all)
   end
+
+  def show
+    @market = Market.find_market(params[:id])
+    if @market.class == Market
+      render json: MarketSerializer.new(@market)
+    else
+      render json: ErrorMarketSerializer.new(@market).not_found, status: 404
+    end
+  end
 end
