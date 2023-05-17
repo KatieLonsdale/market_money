9ee16a061f97be90c3f5a430f14aec239ab4ac05
Katie Lonsdale <katie952@gmail.com>
2023-05-17 13:08:17 -0600

    feat: add find_mv method

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -5,6 +5,10 @@ class MarketVendor < ApplicationRecord
   validates_presence_of :market_id, :vendor_id
   validate :unique_market_vendor
 
+  def self.find_mv(ids)
+    MarketVendor.find_by(market_id: ids[:market_id], vendor_id: ids[:vendor_id])
+  end
+
   def unique_market_vendor
     if exist?
       errors.add(:base, "Market vendor asociation between market with market_id=#{market_id} and vendor_id=#{vendor_id} already exists")
