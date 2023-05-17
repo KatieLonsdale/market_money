f71bdacc7a5d1e6b6ef231705e3359edcd51fdde
Katie Lonsdale <katie952@gmail.com>
2023-05-17 11:54:14 -0600

    feat: add exist? method

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -6,8 +6,12 @@ class MarketVendor < ApplicationRecord
   validate :unique_market_vendor
 
   def unique_market_vendor
-    if MarketVendor.exists?(market_id: market_id, vendor_id: vendor_id)
+    if exist?
       errors.add(:base, "Market vendor asociation between market with market_id=#{market_id} and vendor_id=#{vendor_id} already exists")
     end
+  end
+
+  def exist?
+    MarketVendor.exists?(market_id: market_id, vendor_id: vendor_id)
   end
 end
