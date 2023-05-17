37253a6ddc27f4eb4053d0241934016fb4d84308
Katie Lonsdale <katie952@gmail.com>
2023-05-17 10:24:24 -0600

    test: that custom 404 error message is returned if vend/market does not exist

--- spec/requests/api/v0/market_vendors_requets.rb
+++ spec/requests/api/v0/market_vendors_requets.rb
@@ -24,5 +24,42 @@ RSpec.describe 'Market Vendors API' do
       expect(MarketVendor.first.market_id).to eq(market_1.id)
       expect(MarketVendor.first.vendor_id).to eq(vendor_1.id)
     end
+    it 'returns a custom error 404 message if market does not exist' do
+      vendor_1 = create(:vendor)
+      mv_params = ({
+                    "market_id": 1,
+                    "vendor_id": vendor_1.id
+                  })
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post '/api/v0/market_vendors', headers: headers, params: JSON.generate(market_vendor: mv_params)
+
+      expect(response.status).to eq(404)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data).to have_key(:errors)
+      expect(data[:errors][0]).to have_key(:detail)
+      expect(data[:errors][0][:detail]).to eq("Validation failed: Market must exist")
+    end
+
+    it 'returns a custom error 404 message if vendor does not exist' do
+      market_1 = create(:market)
+      mv_params = ({
+                    "market_id": market_1.id,
+                    "vendor_id": 1
+                  })
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post '/api/v0/market_vendors', headers: headers, params: JSON.generate(market_vendor: mv_params)
+
+      expect(response.status).to eq(404)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data).to have_key(:errors)
+      expect(data[:errors][0]).to have_key(:detail)
+      expect(data[:errors][0][:detail]).to eq("Validation failed: Vendor must exist")
+    end
   end
 end
