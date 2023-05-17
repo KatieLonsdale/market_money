7f34e0b92a59134a297330ac733a2b51c95a243c
Katie Lonsdale <katie952@gmail.com>
2023-05-17 11:43:13 -0600

    feat: add unique_market_vendor custom validation

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -3,4 +3,11 @@ class MarketVendor < ApplicationRecord
   belongs_to :vendor
 
   validates_presence_of :market_id, :vendor_id
+  validate :unique_market_vendor
+
+  def unique_market_vendor
+    if MarketVendor.exists?(market_id: market_id, vendor_id: vendor_id)
+      errors.add(:base, "Market vendor asociation between market with market_id=#{market_id} and vendor_id=#{vendor_id} already exists")
+    end
+  end
 end
