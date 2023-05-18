d3bf3b3296584ff7d0eaee3fe62df980dcd0c6d5
Katie Lonsdale <katie952@gmail.com>
2023-05-18 13:49:49 -0600

    feat: add distance attribute

--- app/poros/atm.rb
+++ app/poros/atm.rb
@@ -1,20 +1,15 @@
-include Geocoder
-
 class Atm
   attr_reader :name,
               :address,
               :lat,
-              :lon
+              :lon,
+              :distance
 
   def initialize(data)
     @name = 'ATM'
     @address = data.dig(:address, :freeformAddress)
     @lat = data.dig(:position, :lat)
     @lon = data.dig(:position, :lon)
-  end
-
-  def distance(market_id)
-    market = Market.find(market_id)
-    Geocoder::Calculations.distance_between([@lat, @lon], [market.lat, market.lon])
+    @distance = data[:dist]
   end
 end
