13391640105870a15e4465a8093308c6c4340593
Katie Lonsdale <katie952@gmail.com>
2023-05-17 18:06:18 -0600

    feat: add search method

--- app/models/market.rb
+++ app/models/market.rb
@@ -11,6 +11,25 @@ class Market < ApplicationRecord
     end
   end
 
+  def self.search(params)
+    if params[:state].present? && !params[:state].empty?
+      results = Market.where('lower(state)=lower(?)', params[:state])
+      if params[:city].present? && !params[:state].empty?
+        results = results.where('lower(city)=lower(?)', params[:city])
+          if params[:name].present? && !params[:city].empty?
+            results = results.where('lower(name)=lower(?)', params[:name])
+          end
+      end
+    elsif !params[:state].present? && !params[:city].present?
+      if params[:name].present? && !params[:name].empty?
+        results = Market.where('lower(name)=lower(?)', params[:name])
+      end
+    else
+      results = ErrorMarket.new("Invalid set of parameters. Please provide a valid set of parameters to perform a search with this endpoint.")
+    end
+    results
+  end
+
   def vendor_count
     vendors.count
   end
