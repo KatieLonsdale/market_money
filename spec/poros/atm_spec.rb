bf0784733792f2ad2ea32949ff82d4dd63400cfc
Katie Lonsdale <katie952@gmail.com>
2023-05-18 13:37:17 -0600

    test: add distance method tests

--- spec/poros/atm_spec.rb
+++ spec/poros/atm_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe Atm do
       expect(atm.lon).to eq(-106.029628)
     end
   end
+
+  describe 'distance' do
+    it 'returns the distance of an atm from a market' do
+      market = create(:market, lat: '35.077529', lon: '-106.600449')
+      atm = Atm.new(mock_atm)
+
+      expect(atm.distance(market.id)).to eq(32.680908220435576)
+    end
+  end
 end
