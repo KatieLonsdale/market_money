749d56dea4a254aaea7647e4305625281dd87d25
Katie Lonsdale <katie952@gmail.com>
2023-05-18 13:48:21 -0600

    test: add test for distance attribute

--- spec/poros/atm_spec.rb
+++ spec/poros/atm_spec.rb
@@ -9,15 +9,7 @@ RSpec.describe Atm do
       expect(atm.address).to eq('820 Route 66, Moriarty, NM 87035')
       expect(atm.lat).to eq(35.004683)
       expect(atm.lon).to eq(-106.029628)
-    end
-  end
-
-  describe 'distance' do
-    it 'returns the distance of an atm from a market' do
-      market = create(:market, lat: '35.077529', lon: '-106.600449')
-      atm = Atm.new(mock_atm)
-
-      expect(atm.distance(market.id)).to eq(32.680908220435576)
+      expect(atm.distance).to eq(2748.404939)
     end
   end
 end
