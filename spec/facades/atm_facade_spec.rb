1c7cdd76ae143f406f2fb06162b9895c19e21c3d
Katie Lonsdale <katie952@gmail.com>
2023-05-18 15:07:40 -0600

    test: closest_atms method and helper methods

--- spec/facades/atm_facade_spec.rb
+++ spec/facades/atm_facade_spec.rb
@@ -3,25 +3,33 @@ require 'rails_helper'
 RSpec.describe AtmFacade do
   describe 'initialize' do
     it 'exists and has a vendor_id' do
-      atm = AtmFacade.new(1)
+      atm_facade = AtmFacade.new(1)
 
-      expect(atm).to be_a(AtmFacade)
-      expect(atm.vendor_id).to eq(1)
+      expect(atm_facade).to be_a(AtmFacade)
+      expect(atm_facade.vendor_id).to eq(1)
     end
   end
 
   describe 'closest_atms' do
-    before(:each) do
-      market = create(:market, lat: '35.077529', lon: '-106.600449')
-      atms = AtmFacade.new(market.id).closest_atms
-    end
     it 'returns an array of Atm objects' do
-      expect(atm.closest_atms).to all be_a(Atm)
+      market = create(:market, lat: '35.077529', lon: '-106.600449')
+      atm_facade = AtmFacade.new(market.id)
+      expect(atm_facade.closest_atms).to all be_a(Atm)
     end
+  end
 
-    it 'sorts by distance from market' do
-      
-      expect(atm.closest_atms)
+  describe 'find_lat' do
+    it 'returns the latitude of a market with a given id' do
+      market = create(:market, lat: '35.077529', lon: '-106.600449')
+      atm_facade = AtmFacade.new(market.id)
+      expect(atm_facade.find_lat).to eq('35.077529')
+    end
+  end
+  describe 'find_lon' do
+    it 'returns the longitude of a market with a given id' do
+      market = create(:market, lat: '35.077529', lon: '-106.600449')
+      atm_facade = AtmFacade.new(market.id)
+      expect(atm_facade.find_lon).to eq('-106.600449')
     end
   end
 end
