c946d232c0b078abd3aade9167fdb438fd71e3e7
Katie Lonsdale <katie952@gmail.com>
2023-05-19 09:28:48 -0600

    feat: add states_sold_in attribute

--- app/serializers/vendor_serializer.rb
+++ app/serializers/vendor_serializer.rb
@@ -1,4 +1,8 @@
 class VendorSerializer
   include JSONAPI::Serializer
   attributes :name, :description, :contact_name, :contact_phone, :credit_accepted
+
+  attribute :states_sold_in do |vendor|
+    vendor.states_sold_in
+  end
 end
